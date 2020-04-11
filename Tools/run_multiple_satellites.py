354de42e0a201fe161ad0192b89c6bc57bb24679
Nobuhiro Funabiki <nobuhiro.funabiki@jpl.nasa.gov>
2020-04-10 22:53:18 -0700

    Read initial conditions of satellites from a csv file

--- Tools/run_multiple_satellites.py
+++ Tools/run_multiple_satellites.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import pandas as pd
+
 
 def main():
 
+    initial_states_table = pd.read_csv(filepath_or_buffer="sat_initial_states.csv", sep=",", header=None)
+    initial_states = initial_states_table.values
+
     file_name = "../InOut/SC_Simple_Multi.txt"
 
-    pos_x_str = "0.0"
-    pos_y_str = "0.0"
-    pos_z_str = "0.0"
+    iSats = 1
+
+    pos_x_str = str(initial_states[0][iSats])
+    pos_y_str = str(initial_states[1][iSats])
+    pos_z_str = str(initial_states[2][iSats])
     position_str = pos_x_str + "  " + pos_y_str + "  " + pos_z_str + " !  Pos wrt Formation (m), expressed in F\n"
 
-    vel_x_str = "0.0"
-    vel_y_str = "0.0"
-    vel_z_str = "0.0"
+    vel_x_str = str(initial_states[3][iSats])
+    vel_y_str = str(initial_states[4][iSats])
+    vel_z_str = str(initial_states[5][iSats])
     velocity_str = vel_x_str + "  " + vel_y_str + "  " + vel_z_str + " !  Vel wrt Formation (m/s), expressed in F\n"
 
     file = open(file_name, "r")
